15727b560a09e6ddba450a865277cfa5eb21e4ef
vitto-vitto <vic.andre1410@gmail.com>
2021-07-26 21:47:56 -0300

    feature: adicionando algumas rotas

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const http = require('http')
+const { type } = require('os')
 
 http.createServer((req, res) => {
-    res.end('Server online on port 3000')
+    res.writeHead(200, {
+        'Content-Type': 'application ld/json; charset=utf8',
+    })
+    switch(req.url){
+        case '/':
+            return res.end('<h1> This is the home page</h1>');
+        case '/about':
+            return res.end('<h1> This is the about</h1>');
+        case '/product':
+            return res.end('<h1> This is the product page</h1>');
+        default:
+            return res.end(`<h1> The page ${req.url.toLowerCase().replace(/\/| -/g, '')} doesn't exist</h1>`);
+    }
+    res.end('{"code":200, "message": "Server running on port 3000"}')
 }).listen(3000, 'localhost', () => console.log('Server running on port 3000'))
